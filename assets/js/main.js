e798f82faa642d024429bbb2999304c9a5554e09
김유진 <joinyou42995@naver.com>
2023-08-02 15:53:02 +0900

    layout 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
 import { onClickBtn } from "./evtButton.js";
 
-
 window.addEventListener("DOMContentLoaded", () => {
+  const _container = document.querySelector(".container");
+  if (_container.classList.contains("intro")) {
+    fnIntro();
+  } else {
+    fnComm();
+  }
+});
+
+const fnIntro = () => {
+  onClickBtn(".intro-link", (e, _target) => {
+    window.location.href = `${window.location.origin}/html/layout.html`;
+  });
+};
+
+const fnComm = () => {
   onClickBtn(".gnb-toggle-btn", (e, _target) => {
     const _layer = document.querySelector(".gnb-layer");
     if (_target.classList.contains("is-active")) {
@@ -12,4 +26,4 @@ window.addEventListener("DOMContentLoaded", () => {
       _layer.classList.add("is-show");
     }
   });
-});
+};

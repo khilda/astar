59feeae566b5c2dc4e8d06ff75a4fe606b1d7ae0
khilda <joinyou42995@naver.com>
2023-08-25 23:56:49 +0900

    scroll to section

--- assets/js/pages/business.js
+++ assets/js/pages/business.js
@@ -1,14 +1,13 @@
 export const fnBusiness = () => {
   if (!document.querySelector(".business")) return;
-  const links =
-    document.querySelectorAll(".btn-link[data-section]") ||
-    document.querySelectorAll(".btn-link-type2[data-section]");
+  const links = document.querySelectorAll("[data-section]");
   if (!links.length) return;
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
       const id = link.dataset.section;
       const _target = document.getElementById(id);
-      window.pageAnimation.goToSection(_target);
+      console.log(_target)
+      window.pageAnimation.scrollToSection(_target);
     });
   });
 };

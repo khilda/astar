e8cf3d4d20f685c2ee483a0020a8766c62f6eb5b
khilda <joinyou42995@naver.com>
2023-08-30 16:11:52 +0900

    contactus

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@ import { fnIntro } from "./pages/intro.js";
 import { fnMain } from "./pages/main.js";
 import { fnAbout } from "./pages/about.js";
 import { fnBusiness } from "./pages/business.js";
-import { fnCompany } from "./pages/company.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   fnVisual();
@@ -18,7 +17,6 @@ window.addEventListener("DOMContentLoaded", () => {
   fnAbout();
   fnMain();
   fnBusiness();
-  fnCompany();
 });
 
 const fnComm = () => {

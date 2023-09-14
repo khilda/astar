18646e6fb534dc377d576b8ea606c0e91ba4160b
doraaan89 <doraaan89@gmail.com>
2023-09-14 21:45:53 +0900

    slide add features

--- assets/js/pages/business.js
+++ assets/js/pages/business.js
@@ -16,5 +16,45 @@ export const fnBusiness = () => {
     });
   });
 
-  contentsBounce("[class^=business]", "[class^=img-desc]", ".logo-group");
+  slide()
+
+  contentsBounce("[class^=business]", ".slide-view", ".img-desc-group:not(.slide .img-desc-group)", ".logo-group");
 };
+
+function slide() {
+  const hasSlideSection = document.querySelector(".has-slide");
+  const slideWrapper = hasSlideSection.querySelector(".slide-wrapper");
+  const slides = slideWrapper.querySelectorAll(".slide");
+  const pagination = hasSlideSection.querySelector(".slide-pagination");
+  const paginationBtns = hasSlideSection.querySelectorAll(".slide-pagination .btn-page");
+  // let autoplay = null
+  let index = 0;
+  
+  function moveSlide() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    slideWrapper.style.transform = `translateX(${index * -slideWidth}px)`
+  }
+
+  pagination.addEventListener("click", () => {
+    slides.forEach(slide => {
+      const transitionGroup = slide.querySelectorAll(['.img-visual', '.vision-text', '.vision-text h3', '.vision-text p', '.vision-text .btn-move'])
+      const transitionSteps = ['1s', '1.5s', '2.2s', '2.5s', '2.8s'];
+      Array.from(transitionGroup).forEach((item, index) => {
+        item.style.transitionDelay = `${transitionSteps[index]}`
+        console.log(item.style.transitionDelay)
+      })
+    })
+  }, { once: true });
+  paginationBtns.forEach((btn, idx, btns) => {
+    btn.addEventListener("click", () => {
+      index = idx
+      btns.forEach((el) => el.classList.remove("is-active"));
+      btn.classList.add("is-active");
+
+      slides.forEach((el) => el.classList.remove("is-active"));
+      slides[index].classList.add("is-active");
+      
+      moveSlide()
+    });
+  });
+}

187209e4b53ad6761cfd97c59b0237b1cafad6cc
joinyou <joinyou42995@naver.com>
2023-09-23 23:40:18 +0900

    제플린반영

--- assets/js/func/contentsBounce.js
+++ assets/js/func/contentsBounce.js
@@ -1,4 +1,4 @@
-import { InteractionElement } from '../interactionElement.js'
+import { InteractionElement } from "../interactionElement.js";
 
 /**
  * 해당 elements 를 위, 아래로 움직임
@@ -7,11 +7,10 @@ import { InteractionElement } from '../interactionElement.js'
 
 export function contentsBounce(...selector) {
   const elements = document.querySelectorAll(selector);
-  elements.forEach(element => {
-    new InteractionElement(
-      element,
-      [{ type: 'translate', start: -12, end: 12, direction: 'bottom' }, { iterations: Infinity, duration: 7000 }],
-      [{ type: 'opacity', start: 0.8, end: 1 }, { iterations: Infinity }],
-    )
-  })
-} 
+  elements.forEach((element) => {
+    new InteractionElement(element, [
+      { type: "translate", start: -12, end: 12, direction: "bottom" },
+      { iterations: Infinity, duration: 7000 },
+    ]);
+  });
+}

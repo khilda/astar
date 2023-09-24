fbbaf95eb7be7da04fada68193d39116ab09626c
doraaan89 <doraaan89@gmail.com>
2023-09-24 19:54:30 +0900

    business slider 수정

--- assets/js/pages/business.js
+++ assets/js/pages/business.js
@@ -22,38 +22,27 @@ export const fnBusiness = () => {
 };
 
 function slide() {
-  const hasSlideSection = document.querySelector(".has-slide");
-  const slideWrapper = hasSlideSection.querySelector(".slide-wrapper");
-  const slides = slideWrapper.querySelectorAll(".slide");
-  const pagination = hasSlideSection.querySelector(".slide-pagination");
-  const paginationBtns = hasSlideSection.querySelectorAll(".slide-pagination .btn-page");
+  const slideWrap = document.querySelector(".slide-wrap");
+  const slides = slideWrap.querySelectorAll(".slide");
+  const pagination = document.querySelector(".slide-pagination");
+  const paginationBtns = pagination.querySelectorAll(".btn-page");
   // let autoplay = null
   let index = 0;
   
   function moveSlide() {
     const slideWidth = slides[0].getBoundingClientRect().width;
-    slideWrapper.style.transform = `translateX(${index * -slideWidth}px)`
+    slideWrap.scrollTo({
+      left: parseInt(slideWidth) * index,
+      behavior: 'smooth',
+    })
   }
 
-  pagination.addEventListener("click", () => {
-    slides.forEach(slide => {
-      const transitionGroup = slide.querySelectorAll(['.img-visual', '.vision-text', '.vision-text h3', '.vision-text p', '.vision-text .btn-move'])
-      const transitionSteps = ['1s', '1.5s', '2.2s', '2.5s', '2.8s'];
-      Array.from(transitionGroup).forEach((item, index) => {
-        item.style.transitionDelay = `${transitionSteps[index]}`
-        console.log(item.style.transitionDelay)
-      })
-    })
-  }, { once: true });
   paginationBtns.forEach((btn, idx, btns) => {
     btn.addEventListener("click", () => {
       index = idx
       btns.forEach((el) => el.classList.remove("is-active"));
       btn.classList.add("is-active");
 
-      slides.forEach((el) => el.classList.remove("is-active"));
-      slides[index].classList.add("is-active");
-      
       moveSlide()
     });
   });
